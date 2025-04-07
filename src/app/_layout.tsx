5ce3cc038ffff73c13ff5a95db55486f4578103d
Hersh Rudrawal <45576093+HershR@users.noreply.github.com>
2025-04-06 22:58:56 -0700

    Setup React Native Reusable Package

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,18 +2,63 @@ import { Stack } from "expo-router";
 import "../global.css";
 import { ActivityIndicator, StatusBar } from "react-native";
 import { DateProvider } from "../context/DateContext";
-import { Suspense } from "react";
+import { Suspense, useEffect, useLayoutEffect, useRef, useState } from "react";
 import { SQLiteProvider, openDatabaseSync } from "expo-sqlite";
 import { drizzle } from "drizzle-orm/expo-sqlite";
 import { useMigrations } from "drizzle-orm/expo-sqlite/migrator";
 import migrations from "@/drizzle/migrations";
+import {
+  Theme,
+  ThemeProvider,
+  DefaultTheme,
+  DarkTheme,
+} from "@react-navigation/native";
+import { Platform } from "react-native";
+import { NAV_THEME } from "~/lib/constants";
+import { useColorScheme } from "~/lib/useColorScheme";
 
 export const DATABASE_NAME = "workouts";
+const LIGHT_THEME: Theme = {
+  ...DefaultTheme,
+  colors: NAV_THEME.light,
+};
+const DARK_THEME: Theme = {
+  ...DarkTheme,
+  colors: NAV_THEME.dark,
+};
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from "expo-router";
 
 export default function RootLayout() {
+  //db
   const expoDb = openDatabaseSync(DATABASE_NAME);
   const db = drizzle(expoDb);
   const { success, error } = useMigrations(db, migrations);
+
+  //rnr ui lib
+  const hasMounted = useRef(false);
+  const { colorScheme, isDarkColorScheme } = useColorScheme();
+  const [isColorSchemeLoaded, setIsColorSchemeLoaded] = useState(false);
+
+  useIsomorphicLayoutEffect(() => {
+    if (hasMounted.current) {
+      return;
+    }
+
+    if (Platform.OS === "web") {
+      // Adds the background color to the html element to prevent white background on overscroll.
+      document.documentElement.classList.add("bg-background");
+    }
+    setIsColorSchemeLoaded(true);
+    hasMounted.current = true;
+  }, []);
+
+  if (!isColorSchemeLoaded) {
+    return null;
+  }
   return (
     <>
       <Suspense fallback={<ActivityIndicator size="large" />}>
@@ -23,20 +68,26 @@ export default function RootLayout() {
           useSuspense
         >
           <DateProvider>
-            <StatusBar hidden={true} />
-            <Stack>
-              <Stack.Screen
-                name="(tabs)"
-                options={{ headerShown: false }}
-              ></Stack.Screen>
-              <Stack.Screen
-                name="exercise/[id]"
-                options={{ headerShown: false }}
-              ></Stack.Screen>
-            </Stack>
+            <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
+              <StatusBar hidden={true} />
+              <Stack>
+                <Stack.Screen
+                  name="(tabs)"
+                  options={{ headerShown: false }}
+                ></Stack.Screen>
+                <Stack.Screen
+                  name="exercise/[id]"
+                  options={{ headerShown: false }}
+                ></Stack.Screen>
+              </Stack>
+            </ThemeProvider>
           </DateProvider>
         </SQLiteProvider>
       </Suspense>
     </>
   );
 }
+const useIsomorphicLayoutEffect =
+  Platform.OS === "web" && typeof window === "undefined"
+    ? useEffect
+    : useLayoutEffect;
---
+++ src/lib/icons/CircleArrowRight.ts
@@ -0,0 +1,4 @@
+import { CircleArrowRight } from "lucide-react-native";
+import { iconWithClassName } from "./iconWithClassName";
+iconWithClassName(CircleArrowRight);
+export { CircleArrowRight };
