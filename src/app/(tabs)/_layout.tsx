f9571f8e4cb75a649d51ee0b24564bc8879f1a02
Hersh Rudrawal <45576093+HershR@users.noreply.github.com>
2025-04-12 18:17:14 -0700

    Updated Tab Bar colors

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,32 +1,36 @@
-import { View, Text, Image, ImageSourcePropType } from "react-native";
+import { View } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
-import { icons } from "@/src/constants/icons";
+import { Text } from "@/src/components/ui/text";
+import { Search } from "~/lib/icons/Search";
+import { House } from "~/lib/icons/House";
+import { LucideIcon } from "lucide-react-native";
 
+import { useColorScheme } from "@/src/lib/useColorScheme";
+import { NAV_THEME } from "~/lib/constants";
 interface TabIconProps {
   title: string;
-  imgSrc: ImageSourcePropType;
+  Icon: LucideIcon;
   focused: boolean;
 }
 
-const TabIcon = ({ title, imgSrc, focused }: TabIconProps) => {
+const TabIcon = ({ title, Icon, focused }: TabIconProps) => {
   if (focused)
     return (
       <View className="flex flex-1 flex-row w-full min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden">
-        <Image source={imgSrc} tintColor={"#151212"} className="size-5" />
-        <Text className="text-secondary text-base font-semibold ml-2">
-          {title}
-        </Text>
+        <Icon className="color-primary" size={20} />
+        <Text className="font-semibold ml-2">{title}</Text>
       </View>
     );
   return (
     <View className="size-full justify-center items-center mt-4 rounded-full">
-      <Image source={imgSrc} tintColor={"#A8B5DB"} className="size-5"></Image>
+      <Icon className="color-border" size={20} />
     </View>
   );
 };
-
 const _Layout = () => {
+  const { colorScheme, isDarkColorScheme } = useColorScheme();
+
   return (
     <Tabs
       screenOptions={{
@@ -39,7 +43,11 @@ const _Layout = () => {
         },
         tabBarHideOnKeyboard: true,
         tabBarStyle: {
-          backgroundColor: "#2A2E3C",
+          backgroundColor: `${
+            isDarkColorScheme
+              ? NAV_THEME.dark.background
+              : NAV_THEME.light.background
+          }`,
           position: "absolute",
           overflow: "hidden",
         },
@@ -51,7 +59,7 @@ const _Layout = () => {
           title: "Home",
           headerShown: false,
           tabBarIcon: ({ focused }) => (
-            <TabIcon title="Home" imgSrc={icons.home} focused={focused} />
+            <TabIcon title="Home" Icon={House} focused={focused} />
           ),
         }}
       />
@@ -61,7 +69,7 @@ const _Layout = () => {
           title: "Search",
           headerShown: false,
           tabBarIcon: ({ focused }) => (
-            <TabIcon title="Search" imgSrc={icons.search} focused={focused} />
+            <TabIcon title="Search" Icon={Search} focused={focused} />
           ),
         }}
       />
