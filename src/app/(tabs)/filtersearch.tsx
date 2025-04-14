abcdf728642d6c6f0f25f3f285305b60ad9bd8c4
Hersh Rudrawal <45576093+HershR@users.noreply.github.com>
2025-04-13 20:37:49 -0700

    Added remaining pages template
    Added profile, stats and filtersearch tabs pages

---
+++ src/app/(tabs)/filtersearch.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const FilterSearch = () => {
+  return (
+    <View>
+      <Text>FilterSearch</Text>
+    </View>
+  );
+};
+
+export default FilterSearch;
