d40f3ab4db2b7beadae21293ca1dee3a814edb7f
Hersh Rudrawal <45576093+HershR@users.noreply.github.com>
2025-04-27 17:46:42 -0700

    Extracted search page to component

--- src/app/(tabs)/search.tsx
+++ src/app/(tabs)/search.tsx
@@ -16,107 +16,36 @@ import {
 import { Text } from "@/src/components/ui/text";
 import SafeAreaWrapper from "@/src/components/SafeAreaWrapper";
 import ExerciseList from "@/src/components/lists/ExerciseList";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/src/components/ui/tabs";
+import ExerciseNameSearch from "@/src/components/search/ExerciseNameSearch";
 const Search = () => {
-  const [searchQuery, setSearchQuery] = useState("");
-  const [category, setCategory] = useState<string>("");
-  const {
-    data: exercieSuggestions,
-    loading: loading,
-    error: error,
-    refetch: loadExercise,
-    reset,
-  } = useFetch(() => searchExercise({ query: searchQuery }), false);
-
-  useEffect(() => {
-    const timeoutId = setTimeout(async () => {
-      if (searchQuery.trim()) {
-        await loadExercise();
-      } else {
-        reset();
-      }
-    }, 500);
-    return () => clearTimeout(timeoutId);
-  }, [searchQuery]);
-
-  const selectItems = () => {
-    const items = Object.entries(wgerCategories).map(([key, value]) => {
-      return <SelectItem key={key} label={value} value={value}></SelectItem>;
-    });
-    return items;
-  };
-
-  function suggestionToExercise(
-    suggestions: ExerciseSuggestion[]
-  ): Omit<Exercise, "is_favorite">[] {
-    const exercies = suggestions.map((x) => {
-      return {
-        ...x.data,
-        id: x.data.base_id,
-        image: "https://wger.de/" + x.data.image,
-      };
-    });
-
-    if (!!category) {
-      return exercies.filter((x) => x.category === category);
-    }
-
-    return exercies;
-  }
+  const [value, setValue] = React.useState("search");
 
   return (
     <SafeAreaWrapper>
-      <View className="flex-row w-full justify-center items-center gap-x-2 mb-4">
-        <View className="flex-1">
-          <SearchBar
-            placeholder="Search exercise..."
-            value={searchQuery}
-            onChangeText={(text: string) => {
-              setSearchQuery(text);
-            }}
-            onPress={() => {}}
-          />
-        </View>
-        <Select
-          className="w-[35%] md:w-[25%]"
-          defaultValue={{ value: category, label: "None" }}
-          onValueChange={(item) => setCategory(item!.value)}
-        >
-          <SelectTrigger className="w-full">
-            <SelectValue
-              className="text-foreground text-lg"
-              placeholder="Select Muscle Group"
-            />
-          </SelectTrigger>
-          <SelectContent align="center">
-            <SelectGroup>
-              <SelectItem key={"none"} value={""} label={"None"}></SelectItem>
-              {selectItems()}
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-      </View>
-      <>
-        {!loading &&
-        !error &&
-        searchQuery.trim() &&
-        exercieSuggestions &&
-        exercieSuggestions?.length > 0 ? (
-          <Text className="text-xl text-muted-foreground font-bold">
-            Search Results for <Text className=" text-xl">{searchQuery}</Text>
-          </Text>
-        ) : null}
-      </>
-      {loading ? (
-        <ActivityIndicator size={"large"} color={"#2A2E3C"} className="my-3" />
-      ) : error ? (
-        <Text className="text-destructive px-5 my-3">
-          Error: {error.message}
-        </Text>
-      ) : (
-        <ExerciseList
-          exercises={suggestionToExercise(exercieSuggestions || [])}
-        />
-      )}
+      <Tabs
+        value={value}
+        onValueChange={setValue}
+        className="w-full mx-auto flex-col gap-1.5 items-center"
+      >
+        <TabsList className="flex-row w-full max-w-[400px] ">
+          <TabsTrigger value="search" className="flex-1">
+            <Text>Search</Text>
+          </TabsTrigger>
+          <TabsTrigger value="advanced" className="flex-1">
+            <Text>Advanced</Text>
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="search">
+          <ExerciseNameSearch />
+        </TabsContent>
+        <TabsContent value="advanced"></TabsContent>
+      </Tabs>
     </SafeAreaWrapper>
   );
 };
