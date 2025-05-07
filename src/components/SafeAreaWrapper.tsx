7f084f0246c7a148bb82d19441425c39ec776930
Hersh Rudrawal <45576093+HershR@users.noreply.github.com>
2025-05-06 18:21:07 -0700

    Tab bar no longer hides content under it

--- src/components/SafeAreaWrapper.tsx
+++ src/components/SafeAreaWrapper.tsx
@@ -1,6 +1,8 @@
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import React, { ReactNode } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { twMerge } from "tailwind-merge";
+
 interface Props {
   children?: ReactNode;
   style?: string;
@@ -8,7 +10,10 @@ interface Props {
 const SafeAreaWrapper = ({ children, style }: Props) => {
   return (
     <View className="flex-1 bg-secondary">
-      <SafeAreaView className={`flex-1 mx-8 mt-10 mb-5 md:mx-16 ${style}`}>
+      <SafeAreaView
+        className={twMerge("flex-1 mx-8 mt-5 md:mx-16", style)}
+        //
+      >
         {children}
       </SafeAreaView>
     </View>
