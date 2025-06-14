64f78258e5929c58644874408c5715a027b09235
Hersh Rudrawal <45576093+HershR@users.noreply.github.com>
2025-06-14 10:10:09 -0700

    Updated inactive tab color

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -19,14 +19,14 @@ interface TabIconProps {
 const TabIcon = ({ title, Icon, focused }: TabIconProps) => {
   if (focused)
     return (
-      <View className="flex flex-1 flex-row w-full min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden">
+      <View className="flex-1 flex-row w-full min-w-28 min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden">
         <Icon className="color-primary" size={20} />
         <Text className="font-semibold ml-2">{title}</Text>
       </View>
     );
   return (
     <View className="size-full justify-center items-center mt-4 rounded-full">
-      <Icon className="color-border" size={20} />
+      <Icon className="color-primary/50" size={20} />
     </View>
   );
 };
