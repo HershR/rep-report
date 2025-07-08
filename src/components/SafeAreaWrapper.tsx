8509239fe2fca539941b35c7ef8d4ad4418c8be2
Hersh Rudrawal <45576093+HershR@users.noreply.github.com>
2025-07-08 13:12:20 -0700

    Create header and Safeareawithheader comp

--- src/components/SafeAreaWrapper.tsx
+++ src/components/SafeAreaWrapper.tsx
@@ -1,58 +1,49 @@
 import { View } from "react-native";
 import React, { ReactNode } from "react";
 import {
-  Edges,
   SafeAreaView,
   useSafeAreaInsets,
 } from "react-native-safe-area-context";
-import { useHeaderHeight } from "@react-navigation/elements";
 import { twMerge } from "tailwind-merge";
 import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
 
 interface Props {
   children?: ReactNode;
+  viewStyle?: string;
   hasHeader?: boolean;
   hasTabBar?: boolean;
-  viewStyle?: string;
 }
 const SafeAreaWrapper = ({
   children,
-  hasHeader = false,
-  hasTabBar = false,
+  hasHeader,
+  hasTabBar,
   viewStyle = "",
 }: Props) => {
-  const headerHeight = useHeaderHeight();
+  const insets = useSafeAreaInsets();
   let tabHeight = 0;
   if (hasTabBar) {
     tabHeight = useBottomTabBarHeight();
   }
-  const insets = useSafeAreaInsets();
   //Testing
-  const marginTop = headerHeight + 20;
-  const marginBottom = tabHeight + 20;
 
   const debug = false;
   if (debug) {
-    console.log("SafeAreaWrapper", {
-      headerHeight,
+    console.log("SafeAreaWrapper", insets, {
       tabHeight,
-      marginTop,
-      marginBottom,
     });
   }
   return (
     <View className={twMerge("relative flex-1", debug && "bg-red-500")}>
       <View
         style={{
-          paddingTop: !hasHeader ? insets.top : 0,
-          paddingBottom: !hasTabBar ? insets.bottom : 0,
+          paddingTop: hasHeader ? 0 : insets.top,
+          paddingBottom: insets.bottom + tabHeight,
           paddingLeft: insets.left,
           paddingRight: insets.right,
-          marginTop: marginTop,
-          marginBottom: marginBottom,
         }}
         className={twMerge(
           "flex-1 mx-8 md:mx-16",
+
           viewStyle,
           debug && "bg-blue-500"
         )}
